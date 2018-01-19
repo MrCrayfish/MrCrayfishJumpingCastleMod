b239634bcefaacf2f1dea70d4f1ce47e1a2e17e8
MrCrayfish <mrcrayfish@hotmail.com>
2018-01-19 14:33:02 +1030

    Finalized for release

--- src/main/java/com/mrcrayfish/jumpingcastle/Reference.java
+++ src/main/java/com/mrcrayfish/jumpingcastle/Reference.java
@@ -5,7 +5,7 @@ package com.mrcrayfish.jumpingcastle;
  */
 public class Reference
 {
-    public static final String MOD_ID = "cjcm";
+    public static final String MOD_ID = "jumping_castle";
     public static final String MOD_NAME = "MrCrayfish Jumping Castle Mod";
     public static final String MOD_VERSION = "1.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
